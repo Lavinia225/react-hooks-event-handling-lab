d976a850b6533e1d2f96b78bdf13c4ddc1445ae6
Luna Seifert <thepuddingdeity@gmail.com>
2022-11-19 19:03:22 -0700

    commit tree 44d1a4ff4c7d9525178c8ddfc66fc6f65db1f4d9

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function blur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    function focus(){
+        console.log('Good!')
+    }
+
+    return (
+        <>
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handlePass(){
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <>
+        <input type="password" onChange={handlePass}></input>
+        </>
     )
 }
 
